902428291bb804d6de15e8142976b5598ffd656b
Neyza <neyzapolo24@gmail.com>
2024-02-26 15:23:14 -0500

    Implement budget reducer

--- src/store/slices/budgetsSlice.js
+++ src/store/slices/budgetsSlice.js
@@ -1,15 +1,39 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+export const CATEGORIES = [
+    "housing",
+    "food",
+    "transportation",
+    "utilities",
+    "clothing",
+    "healthcare",
+    "personal",
+    "education",
+    "entertainment",
+];
+
+const initialState = CATEGORIES.map((category) => (
+    {
+        category: category,
+        amount: 0
+    }
+)
+); // output this logic [{category: 'perosnal', amount: 0}, {category: 'food', amount: 0}, ...]
+
 export const budgetsSlice = createSlice({
     name: 'budgets',
-    initialState: {
-        counter: 1
-    },
+    initialState: initialState,
     reducers: {
-        increment: (state, /* action */ ) => {
-            state.counter += 1;
+        updateBudget: (state, action) => {
+            const { category, amount } = action.payload;
+
+            const newBudget = state.find(budget => budget.category === category);
+
+            if (newBudget && newBudget > 0) {
+                newBudget.amount = amount;
+            }
         },
     }
-}); 
+});
 
-export const { increment } = budgetsSlice.actions;
+export const { updateBudget } = budgetsSlice.actions;
